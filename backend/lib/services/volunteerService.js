90af22361be90ae9fcf971e04ca9a05c78e91f87
EslebanPerez <eslebanperez@gmail.com>
2022-05-15 13:38:04 -0500

    Add method to get a volunteer by id

--- backend/lib/services/volunteerService.js
+++ backend/lib/services/volunteerService.js
@@ -6,6 +6,10 @@ class VolunteerService{
         const volunteer = await prisma.volunteer.findMany();
         return volunteer;
     }
+    static async getVolunteerById(id){
+        const volunteer = await prisma.volunteer.findUnique({where: {volunteerId: id}});
+        return volunteer;
+    }
 }
 
 module.exports = VolunteerService;
