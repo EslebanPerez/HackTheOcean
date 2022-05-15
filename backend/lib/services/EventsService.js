6fb70332ddd439f2f6f0997df779acbe3f5ac964
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-05-15 00:24:46 -0500

    Create new method in EventsService

--- backend/lib/services/EventsService.js
+++ backend/lib/services/EventsService.js
@@ -1,5 +1,12 @@
-class EventsService{
+// Require para usar Prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
+class EventsService{
+    static async getEventsById(id){
+        const event = await prisma.events.findUnique({where: {eventId: id}});
+        return event;
+    }
 }
 
 module.exports = EventsService;
