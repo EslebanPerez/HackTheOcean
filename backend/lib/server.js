3108ce005149662fd0cd576356d8d0d2f3e15a64
EslebanPerez <eslebanperez@gmail.com>
2022-05-15 15:22:43 -0500

    Update server.js

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -9,11 +9,6 @@ app.get("/", (req, res) => {
     res.json({ message: "alive" });
 });
 
-app.get("/v1/events/:eventId", async (req,res) => {
-    const id = parseInt(req.params.eventId);
-    const event = await EventsService.getEventsById(id);
-    res.json(event);
-});
 app.get("/v1/volunteer/", async (req,res) => {
     const volunteer = await volunteerService.getAllVolunteers();
     res.json(volunteer);
@@ -73,6 +68,43 @@ app.delete("/v1/volunteer/:volunteerId", async (req, res) => {
     await volunteerService.deleteVolunteer(id);
     return res.json({message: " Volunteer deleted"});
 });
+
+app.get("/v1/events", async (req, res) =>{
+    const event = await EventsService.getAll();
+    res.json(event);
+});
+
+app.get("/v1/events/:eventId", async (req,res) => {
+    const id = parseInt(req.params.eventId);
+    const event = await EventsService.getEventsById(id);
+    res.json(event);
+});
+app.post("/v1/events/", async (req, res) =>{
+    const events = {
+        eventDate: new Date(req.body.eventDate),
+        eventDay: req.body.eventDay,
+        beach: req.body.beach
+    };
+    EventsService.addNewEvents(events);
+    return res.json({message: "Create"});
+});
+
+app.delete("/v1/events/:eventId", async (req, res) => {
+    const id = parseInt(req.params.eventId);
+    await EventsService.deleteEvents(id);
+    return res.json({message: "Suprimido"});
+});
+
+app.put("/v1/events/:eventId", async (req, res) => {
+    const id = parseInt(req.params.eventId);
+    const data = {
+        eventDate:  new Date(req.body.eventDate),
+        beach: req.body.beach
+    };
+    await EventsService.updateEventsById(id,data);
+    return res.json({message: "Actualizado"});
+});
+
 app.listen(port, () => {
     console.log(`Listening to requests on port ${port}`);
 });
