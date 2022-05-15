dbd5fa760eeea32246aae14e003964cb6ef4d1df
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-05-15 12:02:42 -0500

    Add cors to the backend

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+const cors = require("cors");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  express.json({
+    type: "*/*",
+  })
+);
+
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 const EventsService = require("./services/EventsService");
 
@@ -9,42 +23,42 @@ app.get("/", (req, res) => {
   res.json({ message: "alive" });
 });
 
-app.get("/v1/events", async (req, res) =>{
+app.get("/v1/events", async (req, res) => {
   const event = await EventsService.getAll();
   res.json(event);
-})
-
-app.get("/v1/events/:eventId", async (req,res) => {
-    const id = parseInt(req.params.eventId);
-    const event = await EventsService.getEventsById(id);
-    res.json(event);
 });
 
-app.post("/v1/events/", async (req, res) =>{
+app.get("/v1/events/:eventId", async (req, res) => {
+  const id = parseInt(req.params.eventId);
+  const event = await EventsService.getEventsById(id);
+  res.json(event);
+});
+
+app.post("/v1/events/", async (req, res) => {
   const events = {
     eventDate: new Date(req.body.eventDate),
     eventDay: req.body.eventDay,
-    beach: req.body.beach
-  }
+    beach: req.body.beach,
+  };
   EventsService.addNewEvents(events);
-  return res.json({message: "Create"})
-})
+  return res.json({ message: "Create" });
+});
 
 app.delete("/v1/events/:eventId", async (req, res) => {
   const id = parseInt(req.params.eventId);
-  await EventsService.deleteEvents(id)
-  return res.json({message: "Suprimido"})
-})
+  await EventsService.deleteEvents(id);
+  return res.json({ message: "Suprimido" });
+});
 
 app.put("/v1/events/:eventId", async (req, res) => {
   const id = parseInt(req.params.eventId);
   const data = {
-    eventDate:  new Date(req.body.eventDate),
-    beach: req.body.beach
-  }
-  await EventsService.updateEventsById(id,data)
-  return res.json({message: "Actualizado"})
-})
+    eventDate: new Date(req.body.eventDate),
+    beach: req.body.beach,
+  };
+  await EventsService.updateEventsById(id, data);
+  return res.json({ message: "Actualizado" });
+});
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
