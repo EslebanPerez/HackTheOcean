7b77b0cdd434fbddcc5be5891d63f94fa26adc0f
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-05-15 14:09:54 -0500

    Modify tests to run with SQL

--- backend/test/services/EventsService.test.js
+++ backend/test/services/EventsService.test.js
@@ -1,13 +1,33 @@
 const EventsService = require("../../lib/services/EventsService");
 
 describe("Unit tests for EventsService", () => {
-    test("GetAll", async () => {
-        expect(await EventsService.getAll()).toBe(2);
-    });
-    test("Use the events Service", async () => {
-        expect(await EventsService.getEventsById(1).beach).toBe("Caletilla");
-    });
-    test("DeleteEvent",  async() => {
-        expect(await EventsService.deleteEvents(4)).toBe("check")
-    })    
+  test("GetAll", async () => {
+    const testEvents = await EventsService.getAll();
+    expect(testEvents.length).toBe(6);
+  });
+  test("GetEventById", async () => {
+    const testEvent = await EventsService.getEventsById(1);
+    expect(testEvent.beach).toBe("Caletilla");
+  });
+
+  test("addNewEvents, updateEventsById and deleteEvents", async () => {
+    const newEvent = {
+      eventDate: new Date("2022-05-28"),
+      beach: "Playa3",
+    };
+    const data = {
+        eventDate: new Date("2022-05-30"),
+        beach: "Playa4",
+    };
+
+    const result = await EventsService.addNewEvents(newEvent);
+    expect(result.beach).toBe(newEvent.beach);
+
+    const updateEvent = await EventsService.updateEventsById(result.eventId,data);
+    expect(updateEvent.beach).toBe("Playa4");
+
+    const deleteResult = await EventsService.deleteEvents(result.eventId);
+    const testEvent = await EventsService.getEventsById(result.eventId);
+    expect(testEvent).toBeNull();
+  });
 });
